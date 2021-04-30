ec8d0248081facbddb5609df759318df0db2be0a
Tate <tate@transcriptic.com>
2021-04-30 16:01:45 -0700

    add gh actions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,30 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.json",
-  },
+  ignorePatterns: ["dist", "examples"],
   plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
   ],
-  rules: {
-    "@typescript-eslint/prefer-nullish-coalescing": "error",
-    "@typescript-eslint/no-unnecessary-condition": "error",
-    "@typescript-eslint/prefer-optional-chain": "error",
-  },
+  overrides: [
+    {
+      files: ["*.ts{,x}"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        // eslint-disable-next-line no-undef
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.json"],
+      },
+      extends: [
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      rules: {
+        "@typescript-eslint/prefer-nullish-coalescing": "error",
+        "@typescript-eslint/no-unnecessary-condition": "error",
+        "@typescript-eslint/prefer-optional-chain": "error",
+      },
+    },
+  ],
 };
