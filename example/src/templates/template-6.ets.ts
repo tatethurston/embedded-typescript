dd1241f4ac1b8d81d0457b4981107080f2a0c482
Tate Thurston <tatethurston@gmail.com>
2022-10-18 17:26:37 -0700

    extract preserveIndentation (#10)

--- example/src/templates/template-6.ets.ts
+++ example/src/templates/template-6.ets.ts
@@ -23,26 +23,21 @@ export default function (props: Props): string {
     result += "Here is a list of users:\n";
     props.users.forEach(function (user) {
       result += "\n  ";
-      result += (function preserveIndentation(text, indentation) {
-        return text
-          .toString()
-          .split("\n")
-          .map((line, idx) => (idx === 0 ? line : indentation + line))
-          .join("\n");
-      })(renderUser(user), "  ");
+      result += preserveIndentation(renderUser(user), "  ");
       result += "\n";
     });
     result += "\n";
   }
   result +=
     "The indentation level is preserved for the rendered 'partial'.\n\nThere isn't anything special about the 'partial'. Here we used another ets template, but any\nexpression yeilding a multiline string would be treated the same.\n\n  ";
-  result += (function preserveIndentation(text, indentation) {
-    return text
-      .toString()
-      .split("\n")
-      .map((line, idx) => (idx === 0 ? line : indentation + line))
-      .join("\n");
-  })(example, "  ");
+  result += preserveIndentation(example, "  ");
   result += "\n\nThe end!";
   return result;
+}
+
+function preserveIndentation(text: string, indentation: string): string {
+  return text
+    .split("\n")
+    .map((line, idx) => (idx === 0 ? line : indentation + line))
+    .join("\n");
 }
